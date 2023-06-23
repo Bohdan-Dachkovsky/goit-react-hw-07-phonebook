8caca8b9e3acb3ad0adb6a9c6478a94a5cebc3ae
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-23 11:32:46 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -2,26 +2,19 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 axios.defaults.baseURL = 'https://646391b4043c103502a657a0.mockapi.io/contacts';
 export const addUser = createAsyncThunk('task/addUser', async (newUser, thunkAPI) => {
-    try {
-        const response = await axios.post("/contacts", {newUser});
-        return response.data;
-      } catch (e) {
-        return thunkAPI.rejectWithValue(e.message);
-      }
+
+      await axios.post("/contacts", {newUser}).then((response) => response.data)
+      .catch(error => thunkAPI.rejectWithValue(error.message));
+      
 })  
 export const dltUser = createAsyncThunk('task/dltContact', async (itemId, thunkAPI) => {
-    try {
-        const response = await axios.delete(`/contacts/${itemId}`);
-        return response.data;
-      } catch (e) {
-        return thunkAPI.rejectWithValue(e.message);
-      }
+
+      await axios.delete(`/contacts/${itemId}`).then((response) => response.data)
+      .catch(error => thunkAPI.rejectWithValue(error.message));
 })  
 export const searchContacts = createAsyncThunk('task/searchContacts', async (searchId, thunkAPI) => {
-    try {
-        const response = await axios.get(`/contacts/${searchId}`);
-        return response.data;
-      } catch (e) {
-        return thunkAPI.rejectWithValue(e.message);
-      }
+
+     await axios.get(`/contacts/${searchId}`).then((response) => response.data)
+     .catch(error => thunkAPI.rejectWithValue(error.message));
+      
 })  
