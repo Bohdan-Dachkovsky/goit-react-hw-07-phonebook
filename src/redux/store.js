93e942edf6cfc169c64718cf7e2faf0f7add448d
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-23 21:25:43 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import logger from 'redux-logger'
 
 export const store = configureStore({
   devTools: true,
-  reducer: contactReducer,
+  reducer:  contactReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(logger),
 });
